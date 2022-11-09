563e8028ddcbb477c5ca84132427499fe9c2345b
tuannt02 <20522122@gm.uit.edu.vn>
2022-11-09 21:16:16 +0700

    create folder and some components

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import MainLayout from './components/layout/MainLayout';
+import HomePage from './pages/home/HomePage';
+
 function App() {
   return (
-    <div>
-      App
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MainLayout />}>
+          <Route index element={<HomePage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
